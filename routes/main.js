4d9e237f58e54d6a868a913f84fa96fbfe2c42d3
Simon Aumayer <simon.aumayer@aboutlabs.de>
2020-01-23 17:38:29 +0100

    added axios and api request for apod

--- routes/main.js
+++ routes/main.js
@@ -6,12 +6,24 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const axios = require('axios');
+
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+.then(response => {
+    apod = {
+      apodUrl: response.data.url,
+      apodExplanation: response.data.explanation
+    };
+}).catch(error => {
+    console.log(error);
+});
+
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'apod.html'));
+  res.render('apod', {pageTitle: 'APOD', apodUrl: apod.apodUrl, apodExplanation: apod.apodExplanation});
 });
 
 router.get('/another-page', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'another-page.html'));
+  res.render('another-page', {pageTitle: 'Another Page'});
 });
 
 module.exports = router;
