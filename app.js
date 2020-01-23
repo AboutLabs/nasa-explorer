4bf3fbbe937a78f7e79114243ee6dcbe653714ba
Simon Aumayer <simon.aumayer@aboutlabs.de>
2020-01-23 17:34:43 +0100

    set handlebars as template engine

--- app.js
+++ app.js
@@ -7,13 +7,27 @@ const app = express();
 
 const mainRoutes = require('./routes/main');
 
+var expressHbs  = require('express-handlebars');
+
+app.engine(
+    'hbs',
+    expressHbs({
+      extname: 'hbs',
+      defaultLayout: 'main-layout',
+      layoutsDir: 'views/layouts/'
+    })
+);
+
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(mainRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page not found'});
 });
 
 app.listen(3000);
